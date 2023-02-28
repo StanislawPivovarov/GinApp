863dacfdd22d2949bfe9c3a6faffdba0de223817
Stanislaw <photovarov@bk.ru>
2023-02-28 23:49:14 +0300

    feat: add tabs

--- App.tsx
+++ App.tsx
@@ -1,9 +1,38 @@
 import { StatusBar } from "expo-status-bar";
+import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import {SiBuymeacoffee} from '@react-icons/all-files/si/SiBuymeacoffee'
+import {IoMdMenu} from '@react-icons/all-files/io/IoMdMenu'
+import {RiShoppingBasketFill} from '@react-icons/all-files/ri/RiShoppingBasketFill'
+import {RiAccountCircleFill} from '@react-icons/all-files/ri/RiAccountCircleFill'
+
 import Main from "./src/screens/Main";
 import { global } from "./styles";
 
+
 export default function App() {
+  const tabs = [
+    {
+        key: '/home',
+        title: 'Главная',
+        icon: <SiBuymeacoffee />
+    },
+    {
+        key: '/catalog',
+        title: 'Каталог',
+        icon: <IoMdMenu/>
+    },
+    {
+        key: '/cart',
+        title: 'Корзина',
+        icon: <RiShoppingBasketFill/>
+    },
+    {
+        key: '/account',
+        title: 'Аккаунт',
+        icon: <RiAccountCircleFill/>
+    }
+]
   return (
     <View style={global.entryPoint}>
       <Main />
--- src/screens/Main/Main.tsx
+++ src/screens/Main/Main.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 import { ScrollView } from 'react-native'
 import { global } from '../../../styles'
 import Header from '../../components/Header'
+import {Route, Routes, useNavigate, useLocation} from 'react-router-dom'
 
 const Main = () => {
     return(
         <ScrollView style={global.screen}>
-            <Header name='Главная'/>
+            {/* <Header name='Главная'/> */}
         </ScrollView>
     )
 }
