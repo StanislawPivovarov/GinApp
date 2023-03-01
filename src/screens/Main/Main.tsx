c12beeadba77e726c6ceba5e2217f1589b40f069
Stanislaw <photovarov@bk.ru>
2023-03-01 19:32:26 +0300

    feat: remove navigation & add swiper

--- src/screens/Main/Main.tsx
+++ src/screens/Main/Main.tsx
@@ -1,15 +1,34 @@
-import React from 'react'
-import { ScrollView } from 'react-native'
-import { global } from '../../../styles'
-import Header from '../../components/Header'
-import {Route, Routes, useNavigate, useLocation} from 'react-router-dom'
+import React, { useRef } from "react";
+import { ScrollView, View } from "react-native";
+import { global } from "../../../styles";
+import Header from "../../components/Header";
+import { Route, Routes, useNavigate, useLocation } from "react-router-dom";
+import { Swiper, SwiperRef, Toast } from "antd-mobile";
 
 const Main = () => {
-    return(
-        <ScrollView style={global.screen}>
-            <Header name='Главная'/>
-        </ScrollView>
-    )
-}
+    //fetching data from API
+    const colors = ['#ace0ff', '#bcffbd', '#e4fabd', '#ffcfac']
+    const items = colors.map((color, index) => (
+        <Swiper.Item key={index}>
+          <div
+            style={{ background: color }}
+            onClick={() => {
+              Toast.show(`你点击了卡片 ${index + 1}`)
+            }}
+          >
+            {index + 1}
+          </div>
+        </Swiper.Item>
+      ))
+      const ref = useRef<SwiperRef>(null)
+  return (
+    <View style={global.screen}>
+      <Header name="Главная" />
+      <ScrollView >
+        <Swiper autoplay>{items}</Swiper>
+      </ScrollView>
+    </View>
+  );
+};
 
 export default Main;
