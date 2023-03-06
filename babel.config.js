8dd17edb9926cc3f6f9d947c8366e4d03c8e6335
Stanislaw <photovarov@bk.ru>
2023-03-06 09:52:35 +0300

    feat: add navigation

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,7 @@ module.exports = function(api) {
   return {
     presets: ['babel-preset-expo'],
     "plugins": [
-      ["import", { libraryName: "@ant-design/react-native" }]
+      ["import", { libraryName: "@ant-design/react-native"}]
       // The difference with the Web platform is that you do not need to set the style
     ]
   }
--- src/screens/Catalog/Catalog.tsx
+++ src/screens/Catalog/Catalog.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import { ScrollView } from 'react-native'
-import Header from '../../components/Header'
+
 
 const Catalog = () => {
     return(
         <ScrollView>
-            <Header name='Каталог'/>
         </ScrollView>
     )
 }
